73156a33358e5132f2933c69d600a3e8b4f12119
asosnierz <anderson.sosnierz@msn.com>
2020-04-15 00:18:21 -0300

    Em progresso

--- questionario.py
+++ questionario.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Vermelho e branco -> ('\033[0;30;41m'+' o texto'+'\033[0;0m] ])
+
+import os
 '''
 Entrada
 nome
@@ -8,7 +11,22 @@ altura
 peso
 pergunta ele se praticou corrida hoje
 caso sim - distancia percorrida e o tempo
+'''
 
+print ('\033[0;30;43m'+'Seja bem-vindo, precisamos coletar Algumas informações'+'\033[0;0m')
+nome = input('Seja')
+idade = input('Sua idade')
+altura = int(input('Sua Altura'))
+peso = input('Seu peso')
+esporte = input('praticou corrida')
+distancia = input('Qual a distacia vc pecorre')
+print('Seu nome é '+ nome + 'Sua idade é'+ idade + 'Sua Altura é'+ altura)
+    if int(altura)> 1,73:
+         print('Fato interessante: O homem brasileiro tem, em média, 1,73m, e a mulher, 1,60m. Ambos registraram o mesmo crescimento desde 1914: 8,6 cm ')
+    else:
+        print('Fato curioso: Sabia que Sultan Kosen, é o homem mais alto do mundo, medindo 2,51 metros de altura.')
+
+'''
 Saida
 imc
 Caso - media de velocidade praticada
