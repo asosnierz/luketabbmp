4df59c4ce31654a35d55379d0aac1c7c440f51f9
asosnierz <anderson.sosnierz@msn.com>
2020-04-14 22:39:34 -0300

    Inicio de projeto

---
+++ questionario.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Entrada
+nome
+idade
+altura
+peso
+pergunta ele se praticou corrida hoje
+caso sim - distancia percorrida e o tempo
+
+Saida
+imc
+Caso - media de velocidade praticada
+
+
+Criar um repositorio no git
+o codigo - publico
+'''
